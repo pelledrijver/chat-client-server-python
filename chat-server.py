e4bcad2dd5a9adb9658b4540fecc738313d54fb1
Pelle <55585598+pelledrijver@users.noreply.github.com>
2020-05-23 11:46:40 +0200

    Update chat-server.py

--- chat-server.py
+++ chat-server.py
@@ -2,95 +2,100 @@ import socket
 import threading
 
 def on_new_client(clientSocket,address):
-    clientMessage = bytes()
+    clientMessages = bytes()
     username = ""
+    isRunning = True
+    hasHandshake = False
 
-    while True:
-        recievedBytes = clientSocket.recv(4096)
-        if not (recievedBytes):
+    while isRunning:
+        recievedBytes = clientSocket.recv(2)
+        #we used a buffer size of 2 bytes to prove that our protocol 
+        #works even if the original packet is split into multiple messages
+        if not (recievedBytes): # User Disconnected
             try:
                 del clientDict[username]
             except KeyError:
                 pass
-            
             clientSocket.close()
             break
-    
-        if(len(recievedBytes) < 4096):
-            if not recievedBytes.decode("utf-8").endswith("\n"):
-                clientMessage = bytes()
-                response = ("BAD-RQST-BODY\n").encode("utf-8")
-                clientSocket.sendall(response)
 
-        clientMessage = clientMessage + recievedBytes
+        clientMessages = clientMessages + recievedBytes
+        if(clientMessages.decode("utf-8").endswith("\n")):
+            clientMessages = clientMessages.decode("utf-8")
+            messagesArray = clientMessages.strip().split("\n")
+            
+            for clientMessage in messagesArray:
+                clientMessage = clientMessage.strip()
 
-        if(clientMessage.decode("utf-8").endswith("\n")):
-            clientMessage = clientMessage.decode("utf-8")
-            print (address, ' >> ', clientMessage)
-
-            if(clientMessage):
-                if(clientMessage.startswith("HELLO-FROM")):
-                    username = clientMessage[11:].strip()
-                    if not username in clientDict:
-                        clientDict[username] = clientSocket
-                        response = ("HELLO " + username + "\n").encode("utf-8")
-                        clientSocket.sendall(response)
+                if not hasHandshake:
+                    if(clientMessage.startswith("HELLO-FROM") ):
+                        username = clientMessage[11:].strip()
+                        
+                        if not username in clientDict:
+                            clientDict[username] = clientSocket
+                            response = ("HELLO " + username + "\n").encode("utf-8")
+                            hasHandshake = True
+                        else:
+                            clientSocket.sendall(("IN-USE\n").encode("utf-8"))
+                            clientSocket.close()
+                            isRunning = False
+                            break
                     else:
-                        response = ("IN-USE\n").encode("utf-8")
+                        response = "BAD-RQST-HDR\n".encode("utf-8")
                         clientSocket.sendall(response)
-                        clientSocket.close()
-                        break
-                
-                elif(clientMessage.startswith("WHO")):
+                    
+                if(clientMessage.startswith("WHO")):
                     userList = ""
                     for client in clientDict:
                         userList += (client + ", ")
                     userList = userList[:len(userList) - 2] + "\n"
                     response = ("WHO-OK " + userList).encode("utf-8")
-                    clientSocket.sendall(response)
-                
+                    
                 elif(clientMessage.startswith("SEND")):
                     clientMessage = clientMessage[4:].strip()
-                    gapIndex = clientMessage.index(" ")
-                    receiver = clientMessage[:gapIndex]
-                    print("DELIVERY " + username + clientMessage[gapIndex:]) #send this clientMessage to the intended user
+                    try:
+                        (receiver, clientMessage) = clientMessage.split(maxsplit=1)
+                    except ValueError:
+                        receiver = clientMessage
+                        clientMessage = ""
 
                     if receiver in clientDict:
-                        receiverSocket = clientDict[receiver]
-                        if(receiverSocket.sendall(("DELIVERY " + username + clientMessage[gapIndex:] + "\n").encode("utf-8")) == None):
-                            #this checks if the clientMessage was sent succesfully
-                            clientSocket.sendall(("SEND-OK \n").encode("utf-8")) 
+                        if not clientMessage:
+                            response = "BAD-RQST-BODY\n".encode("utf-8")
+                        else:
+                            receiverSocket = clientDict[receiver]
+                            message = ("DELIVERY " + username +" "+clientMessage + "\n").encode("utf-8")
+                            try: 
+                                receiverSocket.sendall(message)
+                                response = "SEND-OK\n".encode("utf-8")
+                            except OSError: 
+                                response = "UNKNOWN\n".encode("utf-8")
                     else:
-                        print("user not found")
-                        response = ("UNKNOWN\n").encode("utf-8")
-                        clientSocket.sendall(response)
+                        response = "UNKNOWN\n".encode("utf-8")
                 else:
-                    response = ("BAD-RQST-HDR\n").encode("utf-8")
-                    clientSocket.sendall(response)
-                clientMessage = bytes()
+                    response = "BAD-RQST-HDR\n".encode("utf-8")
+                
+                clientSocket.sendall(response)
+                clientMessages = bytes()
 
+
+''' TCP server setup '''
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1) 
-host = "127.0.0.1" # Get local machine name
-port = 5378        # Reserve a port for service.
+host = "127.0.0.1"
+port = 5378 
 
-print('Server started!')
-print('Waiting for clients...')
+sock.bind((host, port))
+sock.listen(5)
 
-clientDict = dict()
+clientDict = dict() # Dictionary to store all the clients usernames and addresses
 
-sock.bind((host, port))        # Bind to the port
-sock.listen(5)                 # Now wait for client connection.
-
+''' Main loop: Accepts connection from clients. Each client has its own thread.
+    Maximum amount of connections is 81. '''
 while True:
-        clientSocket, address = sock.accept()     # Establish connection with client.
-        print ('Got connection from', address)
-        if (len(clientDict) < 81):
-            threading.Thread(target=on_new_client, args=(clientSocket,address), daemon=True).start()
-        else:
-            clientSocket.sendall(("BUSY\n").encode("utf-8"))
-            clientSocket.close()
-        
-sock.close()
-
-
+    clientSocket, address = sock.accept()
+    if (len(clientDict) < 81):
+        threading.Thread(target=on_new_client, args=(clientSocket,address), daemon=True).start()
+    else:
+        clientSocket.sendall(("BUSY\n").encode("utf-8"))
+        clientSocket.close()
